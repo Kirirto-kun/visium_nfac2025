5ac75d16d962328d0d92f5961863d8b2aa17fdda
Kirirto-kun <130363723+Kirirto-kun@users.noreply.github.com>
2025-05-04 10:07:00 +0500

    attempt to change image style

---
+++ backend/ghibli.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+GEMINI_API = "AIzaSyAUTV28-Vd8UKyEq6glDn43mBJ_6IqupQA"
+
+client = genai.Client(api_key=GEMINI_API)
+
+image_path = Image.open("phot.jpg")
+
+prompt = "Convert the attached image to a ghibli style art."
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-exp-image-generation",
+    contents=[prompt, image_path],
+    config=types.GenerateContentConfig(
+        response_modalities=["Text", "Image"],
+    )
+)
+
+for part in response.candidates[0].content.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = Image.open(BytesIO(part.inline_data.data))
+        image.save("output_image.png")
+        image.show()
+print(response)
